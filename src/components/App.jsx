3f740654431e7d9663b1aa80dd82e705ec6df013
Alena-Abharian <alenar2531@gmail.com>
2022-09-06 19:25:23 +0300

    task 3.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Searchbar from './searchbar';
+import ImageGallery from './imageGallery';
+
+class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleFormSubmit = (query) => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+
+        }}
+      >
+        <Searchbar onSubmitQuery={this.handleFormSubmit} />
+        <ImageGallery query={this.state.query} />
+      </div>
+    );
+  }
+}
+
+export default App;
